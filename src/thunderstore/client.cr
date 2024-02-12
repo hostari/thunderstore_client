515bb64f198ab14d096d0902f7e646bf2f3077fd
Xavi Ablaza <xavi@hostari.com>
2024-02-12 08:38:34 -0600

    Add more documentation

--- src/thunderstore/client.cr
+++ src/thunderstore/client.cr
@@ -5,18 +5,23 @@ module Thunderstore
     def initialize(@community : String = "")
     end
 
+    # Sets the `@community` instance variable.
+    # You can find a list of communities by going to https://thunderstore.io
+    # and looking at the the subdomains that each mod community is listed under.
     def set_community(community : String)
       @community = community
     end
 
-    # Returns the base url for which this client will make API requests to
+    # Returns the base url for which this client will make API requests to.
     def base_url : URI
       return URI.parse("https://thunderstore.io") if community.empty?
 
       URI.parse("https://#{community}.thunderstore.io")
     end
 
-    def get(path : String)
+    # Initializes an `HTTP::Client` for the configured `base_url`, and executes a GET request on the specified `path`.
+    # The response will have its body as a String, accessed via `HTTP::Client::Response#body`.
+    def get(path : String) : HTTP::Client::Response
       HTTP::Client.new(base_url).get(path)
     end
   end
--- src/thunderstore_client.cr
+++ src/thunderstore_client.cr
@@ -1,6 +1,7 @@
 require "json"
 require "http/client"
 
+# Specifies information related to this shard, like the `ThunderstoreClient::VERSION`
 class ThunderstoreClient
   VERSION = "1.0.2"
 end
