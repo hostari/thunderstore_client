4609d2c9a5874f51c8df9f2e0128289985a2eab2
Xavi Ablaza <xavi@hostari.com>
2024-02-12 09:47:10 -0600

    Bump version to 1.0.3

--- src/thunderstore_client.cr
+++ src/thunderstore_client.cr
@@ -3,7 +3,7 @@ require "http/client"
 
 # Specifies information related to this shard, like the `ThunderstoreClient::VERSION`
 class ThunderstoreClient
-  VERSION = "1.0.2"
+  VERSION = "1.0.3"
 end
 
 require "./thunderstore/**"
